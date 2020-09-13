f5adb190eacc7bf7787a00628337abf35689bb3c
Ivan Moreno <33256758+B16f00t@users.noreply.github.com>
2020-09-13 19:09:55 +0200

    Add files via upload

---
+++ libs/update.py
@@ -0,0 +1,21 @@
+﻿import requests
+import re
+import webbrowser
+from tkinter import messagebox
+
+
+def start(version):
+    """Check if there is a new version"""
+    request = requests.get("https://github.com/B16f00t/whapa")
+    pattern = r'WhatsApp Parser Toolset v(\d.\d*)'
+    matches = re.findall(pattern, request.text)
+    for match in matches:
+        if match:
+            update = match
+            break
+
+    if float(update) > float(version):
+        messagebox.showinfo("Update", "New version available\n{}".format(update))
+        webbrowser.open_new_tab("https://github.com/B16f00t/whapa")
+    else:
+        messagebox.showinfo("Update", "You have the latest version")
