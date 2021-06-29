4b51005eb55948baec04acb327e924eff2c209e5
Ivan Moreno <33256758+B16f00t@users.noreply.github.com>
2021-06-29 22:21:06 +0200

    Support Crypt14 and Crypt12

--- libs/whacipher.py
+++ libs/whacipher.py
@@ -15,11 +15,11 @@ output = ""
 def banner():
     """ Function Banner """
 
-    print("""
+    print(r"""
      __      __.__                  .__       .__                  
     /  \    /  \  |__ _____    ____ |__|_____ |  |__   ___________ 
-    \   \/\/   /  |  \\\\__  \ _/ ___\|  \____ \|  |  \_/ __ \_  __ \\
-     \        /|   Y  \/ __ \\\\  \___|  |  |_> >   Y  \  ___/|  | \/
+    \   \/\/   /  |  \\__  \ _/ ___\|  \____ \|  |  \_/ __ \_  __ \
+     \        /|   Y  \/ __ \\  \___|  |  |_> >   Y  \  ___/|  | \/
       \__/\  / |___|  (____  /\___  >__|   __/|___|  /\___  >__|   
            \/       \/     \/     \/   |__|        \/     \/               
     --------------- Whatsapp Encryption and Decryption -----------------
@@ -37,7 +37,7 @@ def help():
     """)
 
 
-def encrypt(db_file, key_file, db_cript, output):
+def encrypt12(db_file, key_file, db_cript, output):
     """ Function encrypt msgstore Database """
     try:
         with open(key_file, "rb") as fh:
@@ -63,7 +63,7 @@ def encrypt(db_file, key_file, db_cript, output):
         print("[e] An error has ocurred encrypting '" + db_file + "' - ", e)
 
 
-def decrypt(db_file, key_file, path):
+def decrypt14(db_file, key_file, path):
     """ Function decrypt Crypt14 Database """
     try:
         if os.path.getsize(key_file) != 158:
@@ -78,6 +78,30 @@ def decrypt(db_file, key_file, path):
 
         data = db_data[191:]
         iv = db_data[67:83]
+        aes = AES.new(key, mode=AES.MODE_GCM, nonce=iv)
+        with open(path, "wb") as fh:
+            fh.write(zlib.decompress(aes.decrypt(data)))
+        print("[-] " + db_file + " decrypted, '" + path + "' created")
+
+    except Exception as e:
+        print("[e] An error has ocurred decrypting '" + db_file + "' - ", e)
+
+
+def decrypt12(db_file, key_file, path):
+    """ Function decrypt Crypt12 Database """
+    try:
+        if os.path.getsize(key_file) != 158:
+            quit('[e] The specified input key file is invalid.')
+
+        with open(key_file, "rb") as fh:
+            key_data = fh.read()
+
+        key = key_data[126:]
+        with open(db_file, "rb") as fh:
+            db_data = fh.read()
+
+        data = db_data[67:-20]
+        iv = db_data[51:67]
         aes = AES.new(key, mode=AES.MODE_GCM, nonce=iv)
         with open(path, "wb") as fh:
             fh.write(zlib.decompress(aes.decrypt(data)))
@@ -105,8 +129,8 @@ if __name__ == "__main__":
         if args.encrypt:
             if os.path.exists(args.file):
                 if os.path.exists(args.encrypt[0]) and os.path.exists(args.encrypt[1]):
-                    print("[i] Starting to encrypt...")
-                    encrypt(args.file, args.encrypt[0], args.encrypt[1], args.output)
+                    print("[i] Starting to encrypt in Crypt12...")
+                    encrypt12(args.file, args.encrypt[0], args.encrypt[1], args.output)
                 else:
                     print("[e] '" + args.encrypt[0] + "' or '" + args.encrypt[1] + "' doesn't exist")
             else:
@@ -116,7 +140,11 @@ if __name__ == "__main__":
             if os.path.exists(args.file):
                 if os.path.exists(args.decrypt):
                     print("[i] Starting to decrypt...")
-                    decrypt(args.file, args.decrypt, args.output)
+                    if ".crypt12" == os.path.splitext(args.file)[1]:
+                        decrypt12(args.file, args.decrypt, args.output)
+
+                    elif ".crypt14" == os.path.splitext(args.file)[1]:
+                        decrypt14(args.file, args.decrypt, args.output)
 
                 else:
                     print("[e] " + args.decrypt + " doesn't exist")
@@ -132,8 +160,11 @@ if __name__ == "__main__":
                     for crypt_file in files:
                         if ".crypt14" == os.path.splitext(crypt_file)[1]:
                             output = args.output + os.path.splitext(crypt_file)[0]
-                            decrypt(dir + crypt_file, args.decrypt, output)
+                            decrypt14(dir + crypt_file, args.decrypt, output)
 
+                        elif ".crypt12" == os.path.splitext(crypt_file)[1]:
+                                output = args.output + os.path.splitext(crypt_file)[0]
+                                decrypt12(dir + crypt_file, args.decrypt, output)
                     print("[i] Decryption completed")
 
                 else:
